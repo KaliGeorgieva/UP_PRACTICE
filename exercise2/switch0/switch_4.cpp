121e7f04b434914693fdcfeda0124cdc40a2a2df
Kalina Georgieva <kalinarg@uni-sofia.bg>
2015-11-05 16:30:21 +0200

    switch_4
    Да се напише програма, която по зададено реално
    число x намира стойността на един от следните
    изрази:
    y = x 5
    y = sin(x)
    y = cos(x)
    y = exp(x).
    Като при въведено число 1 трябва да се реши първия
    израз, при въведено число 2 трябва да се реши
    втория израз и т.н.

---
+++ exercise2/switch0/switch_4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cout << "Enter number:" << endl;
+    cin >> a;
+
+    (a%2!=0) ? cout << "a%2!=0" << endl : cout << "a%2=0" << endl;
+    (a%3!=0) ? cout << "a%3!=0" << endl : cout << "a%3=0" << endl;
+    (a%4!=0) ? cout << "a%4!=0" << endl : cout << "a%4=0" << endl;
+    (a%5!=0) ? cout << "a%5!=0" << endl : cout << "a%5=0" << endl;
+    (a%6!=0) ? cout << "a%6!=0" << endl : cout << "a%6=0" << endl;
+    (a%7!=0) ? cout << "a%7!=0" << endl : cout << "a%7=0" << endl;
+    (a%8!=0) ? cout << "a%8!=0" << endl : cout << "a%8=0" << endl;
+    (a%9!=0) ? cout << "a%9!=0" << endl : cout << "a%9=0" << endl;
+    (a%10!=0) ? cout << "a%10!=0" << endl : cout << "a%10=0" << endl;
+
+    return 0;
+}
